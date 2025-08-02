6c4d841b8758963cb8946e3015255e446ec4639c
novak <novakmzv@gmail.com>
2025-08-02 16:35:52 -0500

    feat: added batch routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\BatchController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ProductController::class);
+
+Route::apiResource('batches', BatchController::class);
