9159c447c26d11e453992eb8520494b935a1cb25
novak <novakmzv@gmail.com>
2025-08-04 00:31:46 -0500

    feat: create client routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\BatchController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ProductController::class);
 
 Route::apiResource('batches', BatchController::class);
+
+Route::apiResource('clients', ClientController::class);
